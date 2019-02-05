1f891f3d1274dc3ff3300375da9db2ff19350af5
jioo <jio_qzn@yahoo.com>
2019-02-05 12:13:14 +0800

    Added access_token parameter in signalR client

--- src/Client/src/services/broadcast-service.js
+++ src/Client/src/services/broadcast-service.js
@@ -1,7 +1,10 @@
 import * as signalR from '@aspnet/signalr'
+import JwtService from '@/services/jwt-service'
 
 const connection = new signalR.HubConnectionBuilder()
-            .withUrl(process.env.VUE_APP_BASE_URL + 'broadcast')
-            .build();
+    .withUrl(process.env.VUE_APP_BASE_URL + 'broadcast', {
+        accessTokenFactory: () => JwtService.getToken()
+    })
+    .build();
 
 export default connection
