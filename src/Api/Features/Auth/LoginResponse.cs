7cfb16dbfd4ebb7bea0a4875e26ef6e1a12c5632
jioo <jio_qzn@yahoo.com>
2019-02-05 12:05:07 +0800

    Updated access token payloads - Removed user details in login response

--- src/Api/Features/Auth/LoginResponse.cs
+++ src/Api/Features/Auth/LoginResponse.cs
@@ -6,25 +6,10 @@ namespace WebApi.Features.Auth
 {
     public class LoginResponse
     {
-        [JsonProperty("user")]
-        public UserDetails User { get; set; }
-
         [JsonProperty("access_token")]
         public string AccessToken { get; set; }
 
         [JsonProperty("expires_in")]
         public int ExpiresIn { get; set; }
-    }
-
-    public class UserDetails
-    {
-        [JsonProperty("empId")]
-        public Guid EmployeeId { get; set; }
-        [JsonProperty("fullName")]
-        public string FullName { get; set; }
-        [JsonProperty("username")]
-        public string UserName { get; set; }
-        [JsonProperty("roles")]
-        public List<string> Roles { get; set; }
     }
 }
